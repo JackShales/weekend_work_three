99c241551bc9c9aa835c59c734b2cc74b2e7506e
Jack Shales <jackshalesmusic@gmail.com>
2016-07-15 11:14:38 -0500

    add hotel driver code

--- oop/hotel_reservation.rb
+++ oop/hotel_reservation.rb
@@ -25,10 +25,67 @@ class HotelReservation
   end
 end
 
-
 # Write your own driver code below! Make sure your code tests the following:
+
 # The ability to change a room number even after a reservation has already been created
+puts "Testing room_number functionality..."
+puts
+
+res1 = HotelReservation.new(customer_name: "John", 
+                            date: {month: 7, day: 15, year: 2016},
+                            room_number: 514
+                           )
+
+puts "The originial reservation is for room #{res1.room_number}. The guest wants room 614."
+puts
+
+res1.room_number = 614
+
+if res1.room_number == 614
+  puts "PASS, room_number attr_accessor functional"
+else
+  puts "FAIL, room_number attr_accessor not functional"
+end
+
 # The add_a_fridge method
+
+puts
+puts "Testing add_a_fridge..."
+puts
+
+res1.add_a_fridge
+
+if res1.amenities.include? "fridge"
+  puts "PASS, fridge added!"
+else
+  puts "FAIL, no fridge detected."
+end
+
 # The add_a_crib method
+
+puts
+puts "Testing add_a_crib..."
+puts
+
+res1.add_a_crib
+
+if res1.amenities.include? "crib"
+  puts "PASS, crib added!"
+else
+  puts "FAIL, no crib detected."
+end
+
 # The add_a_custom_amenity method
+
+puts
+puts "Testing add_a_custom_amenity..."
+puts
+
+res1.add_a_custom_amenity("cot")
+
+if res1.amenities.include? "cot"
+  puts "PASS, custom amenity added!"
+else
+  puts "FAIL, no custom amenity detected."
+end
 
